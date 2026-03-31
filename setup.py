022060a72a2331d3b9cc1ac70e3b02fa7c31ac9b
Zac Ravichandran <zachary.ravichandran@gmail.com>
2026-03-31 18:15:23 -0400

    rm resources dir from build

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/weights", glob("weights/*")),
         ("share/" + package_name + "/launch", glob("launch/*.py")),
-        ("share/" + package_name + "/resource", glob("resource/*.py")),
         ("share/" + package_name + "/config", glob("config/*.yaml")),
     ],
     install_requires=["setuptools", "rclpy"],
